6e7ee3db90f237813cc2d1fd95acb799d8da602d
marcos <msaorc@hotmail.com>
2025-09-01 08:52:20 -0300

    Criando testes de CustomerCreateUseCase

---
+++ src/usecase/customer/create/create.customer.dto.ts
@@ -0,0 +1,13 @@
+export interface InputCreateCustomerDto {
+    name: string;
+    address: {
+        street: string;
+        city: string;
+        number: number;
+        zip: string;
+    }
+}
+
+export interface OutputCreateCustomerDto {
+    id: string;
+}
---
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,38 @@
+const input = {
+    id: "123",
+    name: "Alexa",
+    address: {
+        street: "Street",
+        city: "City",
+        number: 200,
+        zip: "Zip",
+    }
+}
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    }
+}
+
+describe("Unit test create customer use case", () => {
+    it("should create a customer", async () => {
+        const customerRepository = MockRepository();
+        const customerCreateUseCase = new CustomerCreateUseCase(customerRepository);
+        const output = await customerCreateUseCase.execute(input)
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            address: {
+                street: input.address.street,
+                number: input.address.number,
+                zip: input.address.zip,
+                city: input.address.city,
+            }
+        });
+    })
+})
